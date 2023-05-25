8d18d29cb3582bf0746b712875cb0b58f443c9c2
Bence <bauerbence5@gmail.com>
2023-05-25 17:39:43 +0200

    Refactored scenic score calculation

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@
 #include <vector>
 #include <algorithm>
 
+int getScenicScore(const std::vector<std::vector<int>> &Matrix, int row, int column, int direction, bool isColumn)
+{
+    int scenicScore = 0;
+    int starterValue = isColumn ? column : row;
+    for (int k = starterValue + direction; k < Matrix[0].size() && k >= 0; k += direction)
+    {
+        scenicScore++;
+        int compareValue = isColumn ? Matrix[row][k] : Matrix[k][column];
+        if (compareValue >= Matrix[row][column])
+            return scenicScore;
+    }
+    return scenicScore;
+}
+
 int main(int argc, char **argv)
 {
     std::ifstream inputStream("day8.in");
@@ -89,37 +103,11 @@ int main(int argc, char **argv)
     {
         for (int j = 1; j < pMatrix[0].size() - 1; j++)
         {
-            int latestScenicScore1 = 0;
-            for (int k = i - 1; k >= 0; k--)
-            {
-                latestScenicScore1++;
-                if (pMatrix[k][j] >= pMatrix[i][j])
-                    break;
-            }
-            int latestScenicScore2 = 0;
-            for (int k = i + 1; k < pMatrix.size(); k++)
-            {
-                latestScenicScore2++;
-                if (pMatrix[k][j] >= pMatrix[i][j])
-                    break;
-            }
-            int latestScenicScore3 = 0;
-            for (int k = j - 1; k >= 0; k--)
-            {
-                latestScenicScore3++;
-                if (pMatrix[i][k] >= pMatrix[i][j])
-                    break;
-            }
-            int latestScenicScore4 = 0;
-            for (int k = j + 1; k < pMatrix[0].size(); k++)
-            {
-                latestScenicScore4++;
-                if (pMatrix[i][k] >= pMatrix[i][j])
-                    break;
-            }
-            int currentScenicScore = latestScenicScore1 * latestScenicScore2 * latestScenicScore3 * latestScenicScore4;
-            if (currentScenicScore > pMaxScenicScore)
-                pMaxScenicScore = currentScenicScore;
+            int currentScenicScore = getScenicScore(pMatrix, i, j, -1, false) *
+                                     getScenicScore(pMatrix, i, j, 1, false) *
+                                     getScenicScore(pMatrix, i, j, -1, true) *
+                                     getScenicScore(pMatrix, i, j, 1, true);
+            pMaxScenicScore = std::max(pMaxScenicScore, currentScenicScore);
         }
     }
     std::cout << pMaxScenicScore << std::endl;
